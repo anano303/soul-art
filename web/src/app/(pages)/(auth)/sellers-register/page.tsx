0b1f173acaab3734fce920db9c2b837c6f455cfa
anano303 <124179773+anano303@users.noreply.github.com>
2025-04-06 23:58:35 +0400

    seller-page

--- web/src/app/(pages)/(auth)/sellers-register/page.tsx
+++ web/src/app/(pages)/(auth)/sellers-register/page.tsx
@@ -1,13 +1,22 @@
 import { AuthLayout } from "@/modules/auth/layouts/auth-layout";
 import { SellerRegisterForm } from "@/modules/auth/components/seller-register-form";
+import SellerBenefits from "@/components/sellerBenefits/sellerBenefits";
+import styles from './seller-register.module.css';
 
 export default function SellerRegisterPage() {
   return (
-    <AuthLayout
-      title="Seller Registration"
-      subtitle="Register your store and start selling on SoulArt!"
-    >
-      <SellerRegisterForm />
-    </AuthLayout>
+    <div className={styles.sellerRegisterContainer}>
+      <div className={styles.benefitsSection}>
+        <SellerBenefits/>
+      </div>
+      <div className={styles.formSection}>
+        <AuthLayout
+          title="Seller Registration"
+          subtitle="Register your store and start selling on SoulArt!"
+        >
+          <SellerRegisterForm />
+        </AuthLayout>
+      </div>
+    </div>
   );
 }
