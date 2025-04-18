ee9f25dc0b8d0355803d842ac0afd7b389fe113f
anano303 <124179773+anano303@users.noreply.github.com>
2025-04-19 03:22:50 +0400

    add auctions page

---
+++ web/src/app/(pages)/auction/page.tsx
@@ -0,0 +1,10 @@
+import AuctionPage from './auction-page';
+
+export const metadata = {
+  title: 'აუქციონი | SoulArt',
+  description: 'SoulArt-ის აუქციონი - მალე',
+};
+
+export default function Page() {
+  return <AuctionPage />;
+}
