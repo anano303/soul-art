5bca10ee5b7889c82d911f841f73ede71cc5c51d
anano303 <124179773+anano303@users.noreply.github.com>
2025-04-21 22:36:37 +0400

    Reapply "themes for categories"
    This reverts commit ad228fdb8c7c8f5e3845efbe40df9fc95b949618.

--- web/src/modules/products/components/product-card.tsx
+++ web/src/modules/products/components/product-card.tsx
@@ -7,17 +7,24 @@ import { Product } from "@/types";
 import { AddToCartButton } from "./AddToCartButton";
 import noPhoto from "../../../assets/nophoto.webp";
 import Star from "../../../assets/Images/star.png";
+import Star2 from "../../../assets/Images/startHandMade.png";
 
 interface ProductCardProps {
   product: Product;
+  className?: string;
+  theme?: "default" | "handmade-theme";
 }
 
-export function ProductCard({ product }: ProductCardProps) {
+export function ProductCard({
+  product,
+  className = "",
+  theme = "default",
+}: ProductCardProps) {
   // ვამოწმებთ სურათის ვალიდურობას
   const productImage = product.images?.[0] || noPhoto.src;
 
   return (
-    <div className="product-card">
+    <div className={`product-card ${theme} ${className}`}>
       <Link href={`/products/${product._id}`}>
         <div className="product-image">
           <Image
@@ -34,14 +41,24 @@ export function ProductCard({ product }: ProductCardProps) {
             <h3 className="product-name">{product.name}</h3>
             <div className="product-rating">
               <span style={{ marginRight: "5px" }}>
-                <Image src={Star} alt="rating star" width={16} height={16} />
+                <Image
+                  src={theme === "handmade-theme" ? Star2 : Star}
+                  alt="rating star"
+                  width={16}
+                  height={16}
+                />
               </span>
               <span className="rating-text" style={{ whiteSpace: "nowrap" }}>
                 {product.rating.toFixed(1)} ({product.numReviews})
               </span>
             </div>
           </div>
-          <p style={{ margin: "5px 15px 0px 15px", color: "#153754" }}>
+          <p
+            style={{
+              margin: "5px 15px 0px 15px",
+              color: theme === "handmade-theme" ? "#7d5a35" : "#153754",
+            }}
+          >
             {product.brand}
           </p>
 
