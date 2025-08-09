7940aec90e9f4b8062b1250999c8b6b32c2247e8
anano303 <124179773+anano303@users.noreply.github.com>
2025-08-09 22:08:30 +0400

    Update order.module.ts

--- server/src/orders/order.module.ts
+++ server/src/orders/order.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersController } from './controller/orders.controller';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { User, UserSchema } from '../users/schemas/user.schema';
 import { OrdersService } from './services/orders.service';
 import { StockReservationService } from './services/stock-reservation.service';
 import { ProductsModule } from '@/products/products.module';
@@ -13,6 +14,10 @@ import { UsersModule } from '@/users/users.module';
       {
         name: Order.name,
         schema: OrderSchema,
+      },
+      {
+        name: User.name,
+        schema: UserSchema,
       },
     ]),
     ProductsModule, // This will make the Product model available in the OrdersService
