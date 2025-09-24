ba97ec316172d9c8c80152b833d43c2bf40b2e6a
anano303 <124179773+anano303@users.noreply.github.com>
2025-09-24 21:00:39 +0400

    Revert "build error"
    This reverts commit 50b2b1a5e22be3042886127eae0aec3132f02d4e.

--- web/src/app/offline/page.tsx
+++ web/src/app/offline/page.tsx
@@ -1,4 +1,9 @@
-'use client';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ოფლაინ რეჟიმი - SoulArt | Offline",
+  description: "თქვენ ოფლაინ რეჟიმში ხართ. გთხოვთ შეამოწმოთ ინტერნეტ კავშირი.",
+};
 
 export default function Offline() {
   return (
